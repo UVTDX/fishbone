3cc47b8340bc07f0330ef7cda7cdaac656137713
UVTDX <thevagabond@tuta.io>
2024-09-01 19:26:02 +0300

    .

--- fishbone.c
+++ fishbone.c
@@ -1,30 +1,27 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
 
-struct termios orig_termios;
+void enableRawMode(struct termios *orig_termios) {
+    tcgetattr(STDIN_FILENO, orig_termios);
+    struct termios raw = *orig_termios;
 
+    raw.c_lflag &= ~(ECHO | ICANON);
 
-void disableRawMode() {
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
-
-void enableRawMode() {
-  tcgetattr(STDIN_FILENO, &orig_termios);
-  atexit(disableRawMode);
-
-  struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
-
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+void disableRawMode(struct termios *orig_termios) {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, orig_termios);
 }
-
 
 int main() {
-	enableRawMode();
+  struct termios orig_termios;
+	enableRawMode(&orig_termios);
 
+  FILE *file = fo
 	char ch;
 	while (read(STDIN_FILENO, &ch, 1) == 1 && ch != 'q');
 	return 0; /* indicates successful execution */
