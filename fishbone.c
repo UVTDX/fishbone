35fd3ad66554dcebdf3949b37b345470cd40fba8
UVTDX <thevagabond@tuta.io>
2024-09-01 19:08:54 +0300

    .

--- fishbone.c
+++ fishbone.c
@@ -1,6 +1,20 @@
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
 
+void enableRawMode() {
+	struct termios raw;
+
+	tcgetattr(STDIN_FILENO, &raw);
+
+	raw.c_lflag &= ~(ECHO);
+
+	tcsetattr(STDIN_FILENO, TCSAFLUSH, & raw);
+}
+
 int main() {
+	enableRawMode();
+
 	char ch;
 	while (read(STDIN_FILENO, &ch, 1) == 1 && ch != 'q');
 	return 0; /* indicates successful execution */
