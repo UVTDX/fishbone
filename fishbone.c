ae2d0e3689cab569cc56ba5a0ff54ff83aa1f798
UVTDX <thevagabond@tuta.io>
2024-09-01 19:38:51 +0300

    .

--- fishbone.c
+++ fishbone.c
@@ -15,7 +15,7 @@ void enableRawMode(struct termios *orig_termios) {
 
 
 void disableRawMode(struct termios *orig_termios) {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, orig_termios);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, orig_termios); /* restore original attr */
 }
 
 
@@ -23,12 +23,14 @@ int main() {
   struct termios orig_termios;
 	enableRawMode(&orig_termios);
 
+  /* open file for writing */
   FILE *file = fopen("output.txt", "w");
   if (file == NULL) {
     perror("Failed to open file");
     exit(EXIT_FAILURE);
   }
 
+  /* read and write input until ESC is pressed */
 	char ch;
 	while (read(STDIN_FILENO, &ch, 1) == 1 && ch != 27) { /* ASCII for ESC key */
     fputc(ch, file);
