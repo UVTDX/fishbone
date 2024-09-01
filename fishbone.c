68c76ed14e4ce05c7419c33f6ebd8388d44ed574
UVTDX <thevagabond@tuta.io>
2024-09-01 19:11:18 +0300

    .

--- fishbone.c
+++ fishbone.c
@@ -2,15 +2,6 @@
 #include <termios.h>
 #include <unistd.h>
 
-void enableRawMode() {
-	struct termios raw;
-
-	tcgetattr(STDIN_FILENO, &raw);
-
-	raw.c_lflag &= ~(ECHO);
-
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, & raw);
-}
 
 int main() {
 	enableRawMode();
